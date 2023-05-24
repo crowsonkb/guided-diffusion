4b36b5a332086afbd9d77690a14725a81f8b8b8b
Katherine Crowson <crowsonkb@gmail.com>
2023-05-24 21:50:43 +0000

    Add package to setup.py so I can depend on it elsewhere

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name="guided-diffusion",
+    packages=["guided_diffusion"],
     py_modules=["guided_diffusion"],
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
 )
